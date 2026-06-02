02ee2dab5af879b9a42c6f3dbd960ba6570f72d3
Chhayaaprakashh <chhaayaa06@gmail.com>
2026-06-02 23:57:51 +0530

    Updated backend and deployment configuration

--- Backendd/src/app.js
+++ Backendd/src/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 const io = connectToSocket(server);
 
 // port
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 5000);
 app.use(cors());
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
@@ -34,8 +34,8 @@ app.get("/home", (req, res) => {
 const start = async () => {
     try {
         const connectionDb = await mongoose.connect(
-            "mongodb+srv://bhavya8007_db_user:Bhavya8007@cluster0.39sobxo.mongodb.net/?retryWrites=true&w=majority"
-        );
+  "mongodb://chhayaprakash:Chhaya30@ac-yxj0x79-shard-00-00.9z3la6y.mongodb.net:27017,ac-yxj0x79-shard-00-01.9z3la6y.mongodb.net:27017,ac-yxj0x79-shard-00-02.9z3la6y.mongodb.net:27017/videoDB?ssl=true&replicaSet=atlas-jconq6-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Chhaya"
+);
 
         console.log(`MONGO CONNECTED DB HOST: ${connectionDb.connection.host}`);
 
--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,7 @@
 let IS_PROD = true;
+
 const server = IS_PROD ?
     "https://videoconfreefinalbackend.onrender.com" :
-
-    "http://localhost:3000"
-
+    "http://localhost:5000"
 
 export default server;
